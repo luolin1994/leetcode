4f959ed41ec41cf2552d3fc97c1f35c9fa41e12a
root <root@localhost.localdomain>
2019-12-24 12:59:29 +0800

    initial

--- linked-list/deleteNode.py
+++ linked-list/deleteNode.py
@@ -1,3 +1,4 @@
+# --coding:utf8--
 # 237 删除链表中的节点
 #请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
 #说明:链表至少包含两个节点。
@@ -32,5 +33,6 @@ node4.next = node5
 solution = Solution()
 solution.deleteNode(node2)
 print(node1.next.val)
+
 
 
